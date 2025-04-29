b1eee0a22410b82c5686af09bc23a0340f19c7f2
Emil Dmitruk <e.dmitruk@herts.ac.uk>
2025-04-29 10:40:57 +0100

    feat: init for Neuron module

---
+++ src/Neuron.jl
@@ -0,0 +1,66 @@
+module NeuronModule
+
+export Neuron
+
+
+"enum-type.jl"  |> include
+using .MyTypes: NeuronType, LIF, ADX, IZH
+
+include("ComputationalUnit.jl")
+using .CompUnit
+
+struct Neuron
+    voltage::Float64
+    adaptation::Float64
+    exConductance::Float64
+    inConductance::Float64
+    synapticDelayBuffer::Int
+    delayCount::Int
+    refrectoryPeriod::Int
+    cUnit::ComputationalUnit
+    spikeBitmap::Vector{Int}  # Assuming spikeBitmap is an integer vector
+    voltageBuffer::Vector{Float64}
+
+    function Neuron(neuronalType::NeuronType)
+        @warn "Those two neuronal types are the same upon construction"
+        voltage = -70.0
+        adaptation = 0.0
+        exConductance = 0.0
+        inConductance = 0.0
+        synapticDelayBuffer = 0
+        delayCount = 0
+        refrectoryPeriod = 0
+        spikeBitmap = Int[]
+        voltageBuffer = Float64[]
+        cUnit = ComputationalUnit(neuronalType)
+
+        return new(
+            voltage,
+            adaptation,
+            exConductance,
+            inConductance,
+            synapticDelayBuffer,
+            delayCount,
+            refrectoryPeriod,
+            cUnit,
+            spikeBitmap,
+            voltageBuffer,
+        )
+        # elseif neuronalType == ADX
+        #     return new(
+        #         voltage,
+        #         adaptation,
+        #         exConductance,
+        #         inConductance,
+        #         synapticDelayBuffer,
+        #         delayCount,
+        #         refrectoryPeriod,
+        #         spikeBitmap,
+        #         voltageBuffer,
+        #     )
+        # else
+        #     throw(ArgumentError("Unknown neuronal type"))
+        # end
+    end
+end
+end # module
