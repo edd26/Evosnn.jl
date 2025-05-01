2f9c8cb0763db574a9f186d4db15bf8a677cfa44
Emil Dmitruk <e.dmitruk@herts.ac.uk>
2025-05-01 09:26:44 +0100

    refactor: reorganise submodules imports and exports; added Ga and util func

--- src/Evosnn.jl
+++ src/Evosnn.jl
@@ -1,48 +1,67 @@
 module Evosnn
 
-export Parameters
-
 export NeuronType, LIF, ADX, IZH
 
-export ComputationalUnit, 
-       resetVoltage!,
-        hasSpike,
-        resetAdaptation,
-        updateAdaptation,
-        updateVoltage,
-        updateExcitatoryCond,
-        updateInhibitoryCond
+export ComputationalUnit,
+    resetVoltage!,
+    hasSpike,
+    resetAdaptation,
+    updateAdaptation,
+    updateVoltage,
+    updateExcitatoryCond,
+    updateInhibitoryCond
 
 export Neuron
-export Individual
 
+export Parameters
+
+export Individual,
+    makeIndividualWithFixedInputOutputConnections
 
 export printIndividualMatrix,
     sumOfConnectionWeights,
     removeLowWeights
 
+# export get_abcdefhXXX_XXXdefghij_Sequence
 
-include("Parameters.jl")
+export Ga,
+    run_Ga!
+
+# ===-===-
+include("enum-type.jl")
+using .MyTypes: NeuronType, LIF, ADX, IZH
+
+include("ComputationalUnit.jl")
+using .CompUnit: ComputationalUnit,
+    resetVoltage!,
+    hasSpike,
+    resetAdaptation,
+    updateAdaptation,
+    updateVoltage,
+    updateExcitatoryCond,
+    updateInhibitoryCond
 
 include("Neuron.jl")
 using .NeuronModule: Neuron
 
-# include("enum-type.jl")
-using .MyTypes: NeuronType, LIF, ADX, IZH
-
-include("ComputationalUnit.jl")
-using .CompUnit: ComputationalUnit, 
-       resetVoltage!,
-        hasSpike,
-        resetAdaptation,
-        updateAdaptation,
-        updateVoltage,
-        updateExcitatoryCond,
-        updateInhibitoryCond
-
+include("Parameters.jl")
+using .ParamsModule: Parameters
 
 include("Individual.jl")
+using .IndividualModule
+
 include("utils.jl")
 
+
+include("UtilityFunctions.jl")
+using .UtilityFunctions
+
+include("Ga.jl")
+using .GaModule
+
+
+
+
+# A note to self- if there is a list of dependencies, they have to be imported into main package file, then they have to be references in the subpackage via double dot
 
 end # module Evosnn
